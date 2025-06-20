50152f529291ea4d82f66e2ed6d775561929b0e0
Cesar Hernandez <cehergra1999@gmail.com>
2025-06-19 21:33:19 -0500

    Add files via upload

---
+++ sistema_interactivo.js
@@ -0,0 +1,48 @@
+let lisClientes=[];
+
+function agregarClientes(){
+    //Inicializacion del programa
+    console.log("¡Bienvenido al Sistema Interactivo de Mensajes!");
+    
+    
+    //Captura de datos del programa
+    let nombre = prompt("Por favor, Ingresa tu nombre:");
+    let edad = prompt("Por favor, Ingresa tu edad:");
+
+    //Codigo para validar 
+    if (!nombre || nombre.trim() === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+   
+    //Convertir edad a numero
+    edad = parseInt(edad);
+    
+    //Codigo para Validacion y Mensajes:
+    if (isNaN(edad)){ // NAN -> not a number
+        console.error("Error: Por favor, ingresa una edad valida en numeros");
+        return
+    } else if (edad < 18){
+        alert(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del codigo!`);
+    } else {
+        alert(`Hola ${nombre}, eres mayor de edad. ¡Preparate para grandes oportunidades en el mundo de la programacion!`);
+    }
+    
+    let cliente={};
+    cliente.nombre=nombre;
+    cliente.edad=edad;
+    lisClientes.push(cliente)
+    
+    
+    const lista = document.getElementById("cliente-lista");
+    lista.innerHTML = "";
+
+    lisClientes.forEach((cliente,i) =>{
+        const listitem = document.createElement("li");
+        listitem.textContent = `Nombre:${cliente.nombre} - Edad:${cliente.edad}`;
+        lista.appendChild(listitem);
+    });
+
+    console.log(lisClientes);
+
+}
